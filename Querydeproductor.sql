db3cf40ff5bcef2fdefd94facc38f4626131624f
Luys <44624521+sanchezluys@users.noreply.github.com>
2024-05-30 20:39:52 -0500

    Update Querydeproductor.sql
    mi primera modificacion

--- Querydeproductor.sql
+++ Querydeproductor.sql
@@ -1,6 +1,14 @@
-SELECT id, nombre, marca
+-- Nombre
+-- Fecha
+-- 2024
+-- Que hace?
+SELECT 
+        id, 
+        nombre, 
+        marca
 
-FROM public.productos
+FROM 
+        public.productos
 
 
 ORDER BY nombre ASC 
